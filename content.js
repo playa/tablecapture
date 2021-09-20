23bca6ba6cae6febc2aad993d81cad740076670c
Yuri Barbashov <lolcoltd@gmail.com>
2021-09-20 14:40:22 +0300

    borders for better ocr

--- content.js
+++ content.js
@@ -7,25 +7,37 @@ function SnapShotDOM(target){
     data.height = target.style.height;
     //data.maxWidth = /*get maxWidth property*/;
     //data.maxHeight = /*get maxHeight  property*/;
+    //target.parentElement.style.borderColor = "yellow";
 
-    target.style.overflow = "visible !important";
-    target.style.height = target.scrollHeight + "px";
-    target.style.width = target.scrollWidth + "px";
-    target.parentElement.style.borderColor = "yellow";
+    let tableSelector = "table";
+    if (typeof(target.id) != 'undefined'){
+        tableSelector = tableSelector + "#" + target.id;
+    }
 
-    html2canvas(target).then(function(canvas) {
-            target.style.overflow = data.overflow;
-            target.style.width = data.width;
-            target.style.height = data.height;
-            //target.style.maxWidth = data.maxWidth;
-            //target.style.maxHeight = data.maxHeight;
-            target.parentElement.style.borderColor = "green";
-            var a = document.createElement('a');
-            a.href = canvas.toDataURL("image/png");
-            a.download = document.location.pathname.split("/").at(-1) + ".png";
-            a.click();
-        }
-    );
+    var sheet = document.createElement('style')
+    sheet.innerHTML = tableSelector + " td," + tableSelector + " th {border: 2px solid black;}";
+    
+    sheet.onload = function(){
+        target.style.overflow = "visible !important";
+        target.style.height = target.scrollHeight + "px";
+        target.style.width = target.scrollWidth + "px";
+        html2canvas(target).then(function(canvas) {
+                target.style.overflow = data.overflow;
+                target.style.width = data.width;
+                target.style.height = data.height;
+                //target.style.maxWidth = data.maxWidth;
+                //target.style.maxHeight = data.maxHeight;
+                //target.parentElement.style.borderColor = "green";
+                var a = document.createElement('a');
+                a.href = canvas.toDataURL("image/png");
+                a.download = document.location.pathname.split("/").at(-1) + ".png";
+                a.click();
+                document.body.removeChild(sheet);
+            }
+        );
+    };
+
+    document.body.appendChild(sheet);
 }
 
 
